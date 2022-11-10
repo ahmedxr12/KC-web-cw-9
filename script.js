3b8b309e80d5916b1d074a573844835897d2a6ce
ahmedxr12 <aa0505295@gmail.com>
2022-11-10 18:34:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+let orders = []
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +8,30 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  const newOrder = {
+    name ,
+    id ,
+    phone ,
+    order
+  }
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
+
+  console.log(orders)
 }
+
 
 function load() {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
-
+  orders.forEach(order => {
+    `<div>
+      <h1>${order.name} - ${order.id}</h1>
+      <h3>${order.order}</h3>
+    </div>`
+  });
+  
 }
